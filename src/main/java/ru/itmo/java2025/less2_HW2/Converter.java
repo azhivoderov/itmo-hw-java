23d0ff828cd85c95ff473cde8d679c25c72f6f36
AlekseyZhivoderov <azhivoderov@yandex.ru>
2025-09-28 18:37:52 +0500

    HW2: fix + update

--- src/main/java/ru/itmo/java2025/less2_HW2/Converter.java
+++ src/main/java/ru/itmo/java2025/less2_HW2/Converter.java
@@ -1,6 +1,7 @@
 package ru.itmo.java2025.less2_HW2;
 
+import ru.itmo.java2025.less2_HW2.impl.ConverterUnits;
+
 public interface Converter {
-    double conv(double value, String from, String to);
-    String[] getConverterUnits();
+    double conv(double value, ConverterUnits from, ConverterUnits to);
 }
--- src/main/java/ru/itmo/java2025/less2_HW2/SpringApplicationRunnerHW2.java
+++ src/main/java/ru/itmo/java2025/less2_HW2/SpringApplicationRunnerHW2.java
@@ -3,14 +3,17 @@ package ru.itmo.java2025.less2_HW2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import ru.itmo.java2025.less2.service.Impl.AccountService;
+import ru.itmo.java2025.less2_HW2.impl.ConverterImpl;
+import ru.itmo.java2025.less2_HW2.impl.ConverterUnits;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringApplicationRunnerHW2 {
     public static void main(String[] args) {
        try (ConfigurableApplicationContext applicationContextHW2 = SpringApplication.run(SpringApplicationRunnerHW2.class, args)){
             Converter converter = applicationContextHW2.getBean(Converter.class);
-            System.out.println(converter.conv(10,"CELSIUS","FAHRENHEIT"));
+            System.out.println(converter.conv(10, ConverterUnits.CELSIUS,ConverterUnits.FAHRENHEIT));
        }
     }
 }
