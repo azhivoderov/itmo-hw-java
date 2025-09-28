e81a3b338269436e4552356e84d7ca7bded0f1c8
AlekseyZhivoderov <azhivoderov@yandex.ru>
2025-09-29 02:34:35 +0500

    less3: update2

--- src/main/java/ru/itmo/java2025/less3/TestResource.java
+++ src/main/java/ru/itmo/java2025/less3/TestResource.java
@@ -1,6 +1,5 @@
 package ru.itmo.java2025.less3;
 
-import jakarta.annotation.Resource;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
