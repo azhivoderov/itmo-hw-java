4bdf1fbb3d163cbd7f978157ed789240a74046ff
AlekseyZhivoderov <azhivoderov@yandex.ru>
2025-10-04 13:51:03 +0500

    HW4: +Scaner

--- spring-database/src/main/java/ru/itmo/spring_database/SpringDatabaseApplication.java
+++ spring-database/src/main/java/ru/itmo/spring_database/SpringDatabaseApplication.java
@@ -4,29 +4,50 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import ru.itmo.spring_database.dao.CityDao;
-import java.util.concurrent.ThreadLocalRandom;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class SpringDatabaseApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringDatabaseApplication.class, args);
        CityDao cityDao = applicationContext.getBean(CityDao.class);
-        for (int i=1; i<=10; i++) {
-            cityDao.create((long) i,
-                    "name_ru"+i,
-                    "name_en"+i,
-                    ThreadLocalRandom.current().nextInt(100000, 999999),
-                    ThreadLocalRandom.current().nextInt(1, 99));
-        }
-        cityDao.updateById(2L,
-                "name_ru"+"_TEST",
-                "name_en"+"_TEST",
-                555,
-                555);
 
-        cityDao.deleteById(10L);
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Добавляем город... ");
+       System.out.print("Введите код города: ");
+       long codeCity = scanner.nextLong();
+       System.out.print("Введите название на русском: ");
+       String nameCityRu = scanner.nextLine();
+       System.out.print("Введите название на английском: ");
+       String nameCityEn = scanner.nextLine();
+       System.out.print("Количество населения: ");
+       Integer numberResidentsCity = scanner.nextInt();
+       System.out.print("Код региона: ");
+       Integer codeRegionCity = scanner.nextInt();
+
+       cityDao.create(codeCity,
+                    nameCityRu,
+                    nameCityEn,
+                    numberResidentsCity,
+                    codeRegionCity);
+
+       System.out.println("Корректируем наименование города... ");
+       System.out.print("Введите код города: ");
+       codeCity = scanner.nextLong();
+
+        cityDao.updateById(codeCity,
+                "Тюмень",
+                "Tyumen",
+                850000,
+                72);
+
+       System.out.println("Удаляем город... ");
+       System.out.print("Введите код города: ");
+       codeCity = scanner.nextLong();
+       cityDao.deleteById(codeCity);
 	}
 
 }
